abbe808d3330cfbdbbdc032bf75098d1633dbad5
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2021-02-08 18:33:28 +0100

    Update app.js

--- colour/app.js
+++ colour/app.js
@@ -6,5 +6,5 @@ const color_change_func = () => {
     let rand_hex_code = "#" + rand_color;
     document.body.style.backgroundColor = rand_hex_code;
 
-    color_name.innerHTML = rand_hex_code;
+    color_name.innerHTML = "color:<br>"rand_hex_code;
 }
