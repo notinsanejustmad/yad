92edc0d2a4c8d5321259646ea96a136f4f80015b
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2021-02-09 21:32:58 +0100

    Create pick.js

---
+++ colour/pick.js
@@ -0,0 +1,14 @@
+  
+let color_name = document.getElementById("color_name");
+
+
+const color_change_func = () => {
+    let rand_color = document.getElementById("color_changer");
+    let rand_hex_code = "#" + rand_color;
+    
+    
+    
+    document.body.style.backgroundColor = rand_hex_code;
+
+    color_name.innerHTML = "color:<br>" + rand_hex_code;
+}
