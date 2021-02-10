1035fa1669ef5cc929c3fb0af8d62f9854e0d3d0
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2021-02-10 11:50:30 +0100

    Update pick.js

--- colour/pick.js
+++ colour/pick.js
@@ -4,11 +4,10 @@ let color_name = document.getElementById("color_name");
 
 const color_change_func = () => {
   let rand_color = document.getElementById("colorChoice").value;
-    let rand_hex_code = "#" + rand_color;
     
     
     
-    document.body.style.backgroundColor = rand_hex_code;
+    document.body.style.backgroundColor = rand_color;
 
-    color_name.innerHTML = "color:<br>" + rand_hex_code;
+    color_name.innerHTML = "color:<br>" + rand_color;
 }
