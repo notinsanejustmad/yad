606da497fc2240ca22422995cbb09d88c30c19de
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2021-03-13 21:20:54 +0100

    Update newtime.js

--- misc/time/newtime.js
+++ misc/time/newtime.js
@@ -5,11 +5,11 @@ function myFunction() {
     var mm = getMinutes();
     var MM = getMonth();
     var YYYY = getFullYear();
-    var YYy = YYYY.slice(0, 3);
-    var YYY = YYYY.slice(3, 5);
+    var YYfirst = YYYY.slice(0, 3);
+    var YYsecond = YYYY.slice(3, 5);
     var SS = getSeconds();
     var minus = "-";
     var dabble = ":";
-    document.getElementById(txt).innerHTML = string.concat(DD, minus, HH, dabble, mm, minus, MM, minus, YYY, minus, YYy, minus, SS);
+    document.getElementById(txt).innerHTML = string.concat(DD, minus, HH, dabble, mm, minus, MM, minus, YYsecond, minus, YYfirst, minus, SS);
   }, 500);
 }
