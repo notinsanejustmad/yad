d568a04be78af39e2c7bd5cc7cdbd73ece114d30
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2021-03-13 21:23:05 +0100

    Update newtime.js

--- misc/time/newtime.js
+++ misc/time/newtime.js
@@ -1,13 +1,14 @@
 function myFunction() {
   setInterval(function(){
-  var DD = getDate();
-    var HH = getHours();
-    var mm = getMinutes();
-    var MM = getMonth();
-    var YYYY = getFullYear();
+    var d = new Date();
+  var DD = d.getDate();
+    var HH = d.getHours();
+    var mm = d.getMinutes();
+    var MM = d.getMonth();
+    var YYYY = d.getFullYear();
     var YYfirst = YYYY.slice(0, 3);
     var YYsecond = YYYY.slice(3, 5);
-    var SS = getSeconds();
+    var SS = d.getSeconds();
     var minus = "-";
     var dabble = ":";
     document.getElementById(txt).innerHTML = string.concat(DD, minus, HH, dabble, mm, minus, MM, minus, YYsecond, minus, YYfirst, minus, SS);
