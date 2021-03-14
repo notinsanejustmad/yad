e7dfcf1158ff13b256ff286ac78b9d9694edafdb
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2021-03-14 12:24:59 +0100

    Update hex.js

--- misc/time/hex.js
+++ misc/time/hex.js
@@ -7,7 +7,7 @@ function myFunction() {
     var totalsec = HH * 60 * 60;
     var temp = mm * 60;
     totalsec = totalsec + temp + ss;
-    var total = 86 400;
+    var total = 86400;
     var ini = total / totalsec;
     var newtemp = 65536 / ini;
     var end = newtemp.toString(16).toUpperCase();
