c790f38494cd8adec5fb9fac77a5540f6953b521
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2021-03-14 13:01:57 +0100

    Update hex.js

--- misc/time/hex.js
+++ misc/time/hex.js
@@ -6,13 +6,20 @@ function myFunction() {
     var ss = d.getSeconds();
     var totalsec = HH * 60 * 60;
     var temp = mm * 60;
+    var DD = d.getDay();
+    var dayhex = "hex";
+        if (DD < 16) {
+          DD.toString(16).toUpperCase();
+  dayhex = "0" + dayhex;
+}
+
     totalsec = totalsec + temp + ss;
     var total = 86400;
     var ini = total / totalsec;
     var newtemp = 65536 / ini;
     newtemp = Math.trunc(newtemp);
     var end = newtemp.toString(16).toUpperCase();
-    var hexcol = end;
+    var hexcol = dayhex + end;
     document.body.style.backgroundColor = hexcol;
     document.getElementById("txt").innerHTML = end;
       }, 50);
