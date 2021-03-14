a770ca95546f11dd2588ae053a3bf4fed0951690
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2021-03-14 16:31:56 +0100

    Create 111.js

---
+++ misc/time/111.js
@@ -0,0 +1,28 @@
+    
+function myFunction() {
+  setInterval(function(){
+ var d = new Date();
+    var h = "";
+    var m = "";
+    var s = "";
+    var HH = d.getHours();
+    var mm = d.getMinutes();
+    var SS = d.getSeconds();
+     var minus = "-";
+    var dabble = ":";
+    var dot = "."
+    for (i = -1 , i < HH, i++)
+    {
+      h = h + 1;
+    }
+             for (i = -1 , i < mm, i++)
+    {
+      m = m + 1;
+    }
+        for (i = -1 , i < SS, i++)
+    {
+      s = s + 1;
+    }
+             document.getElementById("txt").innerHTML = h + dabble + m + dabble + s;
+}, 50);
+}
