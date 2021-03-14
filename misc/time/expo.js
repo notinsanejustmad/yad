a7875173ffdf7d9f7f3245b3911f20cf07d684be
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2021-03-14 20:57:54 +0100

    Update expo.js

--- misc/time/expo.js
+++ misc/time/expo.js
@@ -1,29 +1,39 @@
-    
+<!DOCTYPE html>
+<html>
+<body onload="myFunction()">
+
+<p id="txt"></p>
+
+<script>
+ var times = 500;
 function myFunction() {
-  var times = 500;
+var fraction = 0;
+  var times = 1000;
   s = 0;
   var sec = 0;
   var min = 0;
-  var hour = 0;
-  setInterval(function(){
-  var d = new Date();
+  var hour = 20;
+    var d = new Date();
   var HH = d.getHours();
   var mm = d.getMinutes();
   var SS = d.getSeconds();
-    var fraction = (HH / 24) + (mm / (24 * 60)) + (SS / (24 * 60 * 60));
-    var totalsec = (HH * 60 * 60) + (mm * 60) + SS;
-    for (i = -1; i < totalsec; i++){
-      if (sec < 60){
+	if (HH < 12){
+    }
+
+
+while (hour !== HH && sec !== SS && min !== mm){
+fraction += 0.00001157407;
+      if (sec < 59){
         sec += 1;
       }
       else {
         sec = 0;
-        if (min < 60){
+        if (min < 59){
           min += 1;
         }
         else {
           min = 0;
-          if (hour < 24){
+          if (hour < 23){
             hour += 1;
           }
           else {
@@ -31,22 +41,38 @@ function myFunction() {
           }
         }
       }
+      if (fraction == 0){
+        fraction = 0.0000000000000000000001;
+      }
     }
-    fraction -= 0.5;
-    times = times * fraction;
+
+if (hour == HH && sec == SS && min == mm){
+  setInterval(function(){
+
+    if (fraction == 0){
+      fraction = 0.0000000000000000000001;
+    }
     
+      if (fraction < 0.5){
+      times = 1000;
+  times = times * fraction;
+  }
+  else {
+  times = 1000;
+  times = times / fraction;
+  }
    
-     if (sec < 60){
+     if (sec < 59){
         sec += 1;
       }
       else {
         sec = 0;
-        if (min < 60){
+        if (min < 59){
           min += 1;
         }
         else {
           min = 0;
-          if (hour < 24){
+          if (hour < 23){
             hour += 1;
           }
           else {
@@ -57,6 +83,11 @@ function myFunction() {
     
 document.getElementById("txt").innerHTML = hour + ":" + min + ":" + sec;
 
-
 }, times);
 }
+}
+
+</script>
+
+</body>
+</html>
