d3d954ed9224c8ed714e1cd2365bb83f688b0dce
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2021-03-14 12:19:25 +0100

    Create hex.js

---
+++ misc/time/hex.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  setInterval(function(){
+    var d = new Date();
+    var HH = d.getHours();
+    var mm = d.getMinutes();
+    var ss = d.getSeconds();
+    var totalsec = HH * 60 * 60;
+    var temp = mm * 60;
+    totalsec = totalsec + temp + ss;
+    var total = 86 400;
+    var ini = total / totalsec;
+    var newtemp = 65536 / ini;
+    var end = newtemp.toString(16).toUpperCase();
+      }, 500);
+}
