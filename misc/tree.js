bf5760de94bd6c1bcd123047cdd5eae1becda97e
notinsanejustmad <madelinejbf@gmail.com>
2021-04-09 23:10:58 +0200

    tree diagram.

---
+++ misc/tree.js
@@ -0,0 +1,11 @@
+fetch(tree.json)
+  .then(function (response) {
+    // The JSON data will arrive here
+  })
+  .catch(function (err) {
+    // If an error occured, you will catch it here
+  });
+  var mainContainer = document.getElementById("myData");
+  for (var i = 0; i < data.length; i++) {
+  // append each person to our page
+}
