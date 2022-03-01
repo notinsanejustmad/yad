93d3a5a72e1867b3c0aa14a9b20f62672d9f6e1b
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2022-03-01 20:55:44 +0100

    Update med.js

--- misc/time/med.js
+++ misc/time/med.js
@@ -10,7 +10,7 @@ function myFunction() {
     var DD = d.getDate();
     var dayhex = "hex";
 	var unix = d.getTime();
-	var GMT = Math.trunc((unix - 1646164120)/60/60/1000);
+	var GMT = Math.trunc((unix - 1646164120)/60/1000);
 	
 	
 	
