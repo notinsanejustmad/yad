0129e4c8c390bd55c1acf29c0da4c0e3d7736798
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2025-05-07 21:24:02 +0200

    Create rsstest.js

---
+++ misc/rsstest.js
@@ -0,0 +1,18 @@
+const RSS_URL = `https://codepen.io/spark/feed`;
+
+fetch(RSS_URL)
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    console.log(data);
+    const items = data.querySelectorAll("item");
+    let html = ``;
+    items.forEach(el => {
+      html += `
+        <article>
+          <h2>
+            <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+              ${el.querySelector("title").innerHTML}
+            </a>
+          </h2>
+        </article>
