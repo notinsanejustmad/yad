cef078acc4a3ac1c6e8ac44465c63008974c63f0
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2025-05-07 22:10:56 +0200

    Update rsstest.js

--- misc/rsstest.js
+++ misc/rsstest.js
@@ -1,4 +1,5 @@
-const RSS_URL = `https://cors.io/?https://notinsanejustmad.tumblr.com/rss`;
+Access-Control-Allow-Origin: `tumblr.com`;
+const RSS_URL = `https://notinsanejustmad.tumblr.com/rss`;
 fetch(RSS_URL)
   .then(response => response.text())
   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
