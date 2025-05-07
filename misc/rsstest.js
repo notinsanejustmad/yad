81874375c1faea8e34efe1e5b32d18eae970a134
notinsanejustmad <77732668+notinsanejustmad@users.noreply.github.com>
2025-05-07 22:24:40 +0200

    Update rsstest.js

--- misc/rsstest.js
+++ misc/rsstest.js
@@ -1,5 +1,4 @@
 fetch(`https://notinsanejustmad.tumblr.com/rss`)
-  .then(response => response.text())
   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
   .then(data => {
     console.log(data);
